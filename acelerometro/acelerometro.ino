05626b436020bbaee1ac29f5ddc10158d341a63d
thalissonmt <tmt2@cin.ufpe.br>
2019-09-17 04:17:07 -0700

    Add 3D, filtro e calculo dos angulos do acelerometro

--- acelerometro/acelerometro.ino
+++ acelerometro/acelerometro.ino
@@ -5,6 +5,7 @@
 ADXL345 accel;
 
 int16_t ax, ay, az;
+float X_out, Y_out, Z_out, roll, pitch, rollF, pitchF;
 
 void setup() {
     Wire.begin();
@@ -22,14 +23,18 @@ void setup() {
 void loop() {
     // read raw accel measurements from device
     accel.getAcceleration(&ax, &ay, &az);
-
+    X_out = (float)ax/256;
+    Y_out = (float)ay/256;
+    Z_out = (float)az/256;
+    
+    roll = atan(Y_out / sqrt(pow(X_out, 2) + pow(Z_out, 2))) * 180 / PI;
+    pitch = atan(-1 * X_out / sqrt(pow(Y_out, 2) + pow(Z_out, 2))) * 180 / PI;
+    
     // display tab-separated accel x/y/z values
-    Serial.print("accel:\t");
-    Serial.print("aX: ");
-    Serial.print(ax); Serial.print("\t");
-    Serial.print("aY: ");
-    Serial.print(ay); Serial.print("\t");
-    Serial.print("aZ: ");
-    Serial.println(az);
-    delay(500);
+    rollF = 0.94 * rollF + 0.06 * roll;
+    pitchF = 0.94 * pitchF + 0.06 * pitch;
+  
+    Serial.print(rollF);
+    Serial.print("/");
+    Serial.println(pitchF);
 }
